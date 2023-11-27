55fd7d3f0dfa6a206c43036041e1d6fd306f3a18
safiullah-foragy <safiullahforagy@gmail.com>
2023-11-27 20:46:16 +0600

    update

--- java.java
+++ java.java
@@ -1,5 +1,10 @@
 class java {
     public static void main(String[] args) {
+        Integer i=99;
+        while(i--)
+        {
         System.out.println("Hello, World!"); 
+        }
+
     }
 }
