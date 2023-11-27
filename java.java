e2576a02beccb0946024dcb34a8c492c5d4c5c0d
safiullah-foragy <safiullahforagy@gmail.com>
2023-11-27 21:23:28 +0600

    newfeature

--- java.java
+++ java.java
@@ -5,6 +5,8 @@ class java {
         
         System.out.println("Hello, World! this is my project"); 
         System.out.println("this is another test"); 
+        System.out.println("this is feature 2"); 
+        //https://youtu.be/Ez8F0nW6S-w?si=9H82kdIkZgKpntuv
         
 
     }
